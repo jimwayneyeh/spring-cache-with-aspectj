6128b49611ca88220f9bb7521ebf0616b8494e90
Tzu-Wei Yeh <jimwayne@gmail.com>
2024-06-16 14:59:52 +0800

    feat: initialize with spring initializr

---
+++ src/main/java/tw/jimwayneyeh/example/springcache_with_aspectj/SpringcacheWithAspectjApplication.java
@@ -0,0 +1,13 @@
+package tw.jimwayneyeh.example.springcache_with_aspectj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcacheWithAspectjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcacheWithAspectjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/jimwayneyeh/example/springcache_with_aspectj/SpringcacheWithAspectjApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.jimwayneyeh.example.springcache_with_aspectj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcacheWithAspectjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
